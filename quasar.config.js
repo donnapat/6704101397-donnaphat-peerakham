d9854f513adb2b148556071a031f44d1291feef0
donnapat <donnapat31048@gmail.com>
2025-10-08 11:37:02 +0700

    CI/CD

--- quasar.config.js
+++ quasar.config.js
@@ -37,33 +37,29 @@ export default defineConfig((/* ctx */) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
     build: {
       target: {
-        browser: [ 'es2022', 'firefox115', 'chrome115', 'safari14' ],
+        browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
         node: 'node20'
       },
-
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
-      // vueRouterBase,
-      // vueDevtools,
-      // vueOptionsAPI: false,
-
-      // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
-
-      // publicPath: '/',
+      publicPath: '/6704101397-donnaphat-peerakham/',
       // analyze: true,
       // env: {},
-      // rawDefine: {}
+      // rawDefine: {},
       // ignorePublicFolder: true,
       // minify: false,
       // polyfillModulePreload: true,
-      // distDir
-
+      // distDir,
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
-
       // vitePlugins: [
       //   [ 'package-name', { ..pluginOptions.. }, { server: true, client: true } ]
       // ]
     },
+
+    vueRouterMode: 'hash', // available values: 'hash', 'history'
+    // vueRouterBase,
+    // vueDevtools,
+    // vueOptionsAPI: false,
+    // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#devserver
     devServer: {
@@ -74,8 +70,6 @@ export default defineConfig((/* ctx */) => {
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
     framework: {
       config: {},
-
-
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
 
@@ -87,9 +81,7 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: [
-        'Notify',
-      ]
+      plugins: ['Notify']
     },
 
     // animations: 'all', // --- includes all animations
@@ -112,7 +104,7 @@ export default defineConfig((/* ctx */) => {
     // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
     ssr: {
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
         'render' // keep this as last one
@@ -120,7 +112,6 @@ export default defineConfig((/* ctx */) => {
 
       // extendPackageJson (json) {},
       // extendSSRWebserverConf (esbuildConf) {},
-
       // manualStoreSerialization: true,
       // manualStoreSsrContextInjection: true,
       // manualStoreHydration: true,
@@ -128,7 +119,6 @@ export default defineConfig((/* ctx */) => {
 
       pwa: false
       // pwaOfflineHtmlFilename: 'offline.html', // do NOT use index.html as name!
-
       // pwaExtendGenerateSWOptions (cfg) {},
       // pwaExtendInjectManifestOptions (cfg) {}
     },
@@ -160,11 +150,10 @@ export default defineConfig((/* ctx */) => {
     electron: {
       // extendElectronMainConf (esbuildConf) {},
       // extendElectronPreloadConf (esbuildConf) {},
-
       // extendPackageJson (json) {},
 
       // Electron preload scripts (if any) from /src-electron, WITHOUT file extension
-      preloadScripts: [ 'electron-preload' ],
+      preloadScripts: ['electron-preload'],
 
       // specify the debugging port to use for the Electron app when running in development mode
       inspectPort: 5858,
@@ -186,7 +175,6 @@ export default defineConfig((/* ctx */) => {
 
       builder: {
         // https://www.electron.build/configuration/configuration
-
         appId: '6704101397-donnaphat-peerakham'
       }
     },
